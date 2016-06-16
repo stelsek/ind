53cd3cd8f98b5e4a03b8e68a545bd14143be1870
stelsek <kae2045@mail.ru>
2016-06-16 23:34:46 +0300

    Yn

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -26,12 +26,12 @@ namespace ind
             InitializeComponent();
         }
 
-        
+
         void main()
         {
             textBox.Text = "";
-            nomber.Text ="";
-            XXX.Text ="";
+            nomber.Text = "";
+            XXX.Text = "";
             double x = Convert.ToDouble(X0.Text);
             double h = Convert.ToDouble(H.Text);
             double y = Convert.ToDouble(Y0.Text);
@@ -41,16 +41,23 @@ namespace ind
                 temp += 1;
             }
             textBox1.Text = "   x           Yt              Ych";
-            double[] Yn = new double[temp+1];
+            double[] Yn = new double[temp + 1];
             Yn[0] = y;
             int j = 1;
-            for (double i = x; i <= Yn[0] || j < temp; i += h,j++)
+            for (double i = x; i <= Yn[0] || j < temp; i += h, j++)
             {
                 Yn[j] = nahN(Yn[j - 1], i);
-                Yn[j]=Math.Round(Yn[j], 6);
-                textBox.Text += Yn[j].ToString()+"\n";
+                Yn[j] = Math.Round(Yn[j], 6);
+                textBox.Text += Yn[j].ToString() + "\n";
                 nomber.Text += j + "\n";
-                XXX.Text+=  i+"\n";
+                XXX.Text += i + "\n";
             }
         }
+        double nahN(double y, double x)
+        {
+            double N;
+            N = y + triY(Convert.ToDouble(H.Text), monster(x, y));
+            return (N);
+        }
     }
+}
